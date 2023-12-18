edcd0e1e4260b960fd4ab34a2784e04becad11c6
Adil-Ahmadli <adil.ahmadli@proton.me>
2023-12-18 15:14:25 +0300

    feat: selenium added
    for rendering dynamic content i added selenium. I also updated some  selectors.

--- productscraper/productscraper/spiders/product_spider.py
+++ productscraper/productscraper/spiders/product_spider.py
@@ -1,9 +1,20 @@
 import scrapy
 from productscraper.items import ProductItem
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class ProductSpider(scrapy.Spider):
     name = 'product_spider'
+
+    def __init__(self, *args, **kwargs):
+        super(ProductSpider, self).__init__(*args, **kwargs)
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=chrome_options)
 
     def start_requests(self):
         urls = getattr(self, 'urls', None)
@@ -13,21 +24,28 @@ class ProductSpider(scrapy.Spider):
                 yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        self.driver.get(response.url)
+        rendered_body = self.driver.page_source
+
         item = ProductItem()
-        print(response.xpath('//h1').get())
-        item['title'] = response.xpath('//h1[@class="zLHJIGKJ"]/text()[1]').get()
-        item['price'] = response.css('MwknKR4m::text').get()
-        item['price_without_discount'] = response.css('FteoagkF::text').get()
+        selector = scrapy.Selector(text=rendered_body)
+
+        item['title'] = " ".join(selector.xpath('//h1[@class="pr-new-br"]/span/text()').getall())
+        item['price'] = selector.css('.product-price-container span.prc-org::text').get()
+        item['price_without_discount'] = selector.css('.product-price-container span.prc-dsc::text').get()
 
         if item['price'] is None or item['price'] == '':
             item['price'] = item['price_without_discount']
 
-        item['main_image_url'] = response.xpath('//swiper-container[@class="main-carousel"]/swiper-slide['
+        item['main_image_url'] = selector.xpath('//swiper-container[@class="main-carousel"]/swiper-slide['
                                                 '@data-swiper-slide-index="0"]/img/@src').get()
-        item['other_image_urls'] = response.xpath('//swiper-container[@class="main-carousel"]/swiper-slide['
+        item['other_image_urls'] = selector.xpath('//swiper-container[@class="main-carousel"]/swiper-slide['
                                                   '@data-swiper-slide-index!="0"]/img/@src').getall()
 
-        item['rating_score'] = response.css('.rnUHKhce::text').get()
-        item['review_count'] = response.css('.Euo7zJKH span b::text').get()
+        item['rating_score'] = selector.css('.rnUHKhce::text').get()
+        item['review_count'] = selector.css('.Euo7zJKH span b::text').get()
     
         yield item
+
+    def closed(self, reason):
+        self.driver.quit()
