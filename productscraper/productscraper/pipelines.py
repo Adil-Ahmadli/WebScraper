d0faa96120ea33833a0e025f4e6143c8f1c1b61c
Adil-Ahmadli <adil.ahmadli@proton.me>
2023-12-19 20:58:13 +0300

    feat: added pipeline and data cleaning

--- productscraper/productscraper/pipelines.py
+++ productscraper/productscraper/pipelines.py
@@ -10,4 +10,39 @@ from itemadapter import ItemAdapter
 
 class ProductscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        field_names = adapter.field_names()
+        for field in field_names:
+            if field!= "other_image_urls":
+                value = adapter.get(field)
+                if value is not None:
+                    adapter[field] = value.strip()
+
+        value = adapter.get("price_without_discount")
+        if value is None:
+            adapter["price_without_discount"] = 0.0
+        else:
+            value = value.replace(' TL', '')
+            value = value.replace(',', '.')
+            adapter["price_without_discount"] = float(value)
+
+        value = adapter.get("price")
+        if value is None:
+            adapter["price"] = adapter.get("price_without_discount")
+        else:
+            value = value.replace(' TL', '')
+            value = value.replace(',', '.')
+            adapter["price"] = float(value)
+
+        value = adapter.get("rating_score")
+        if value is None:
+            adapter["rating_score"] = 0
+
+        value = adapter.get("review_count")
+        if value is None:
+            adapter["review_count"] = 0
+
+
+
+
         return item
